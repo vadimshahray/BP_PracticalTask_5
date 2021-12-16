a00c1b658dbb74984c311f23ed08460dec46fd93
Вадим Шахрай <vadimshahray.workmail@gmail.com>
2021-12-16 21:35:05 +0700

    Были упрощены переменные, введены новые, в целом код стал проще читаться (1)

--- BP_PracticalTask_5/BP_PracticalTask_5.cpp
+++ BP_PracticalTask_5/BP_PracticalTask_5.cpp
@@ -20,83 +20,73 @@ const size_t Nmax = 16;
 /// <param name="N">Размер передаваемого массива</param>
 /// <param name="X">Повторяемое число</param>
 /// <returns>Размерность массива после его обработки</returns>
-UCHAR DoubleX(short* A, UCHAR N, short X);
+UCHAR DoubleX(UCHAR *A, UCHAR N, short X);
 
 /// <summary>Функция выводит размер массива и все его элементы в порядке возрастания их индексов</summary>
 /// <param name="A">Массив</param>
 /// <param name="N">Размерность массива</param>
-void PrintArray(short* A, UCHAR N);
+void PrintArray(UCHAR *A, UCHAR N);
 
 //<summary>Функция наполняет массив случайными числами</summary>
 //<param name="A">Массив</param>
 //<param name="N">Размерность массива</param>
-void RandArray(short* A, UCHAR N);
+void RandArray(UCHAR *A, UCHAR N);
 
 int main()
 {
    setlocale(LC_ALL, "");
 
-   short A[Nmax] = { }, X = 0;
-   UCHAR N = 0;
+   UCHAR A[Nmax] = { }, N = 0, X = 0;
    time_t t;
 
    printf_s("Введите целые числа N ([1; %zu]) и целые числа X ([%hi; %hi]): ", Nmax, Rmin, Rmax);
-   scanf_s("%hhi %hi", &N, &X);
+   scanf_s("%hhi %hhi", &N, &X);
 
    if (N > Nmax) N = Nmax;
 
-   srand((unsigned)time(&t));
+   srand((unsigned) time(&t));
    RandArray(A, N);
-   PrintArray(A, DoubleX(A, N, X));
+   PrintArray(A, DoubleX(A, N, 7));
 
    return 0 * _getch();
 }
 
-UCHAR DoubleX(short* A, UCHAR N, short X)
+UCHAR DoubleX(UCHAR *A, UCHAR N, short X)
 {
-   // a - кол-во новых элементов
-   UCHAR a = 0;
-
-   for (short i = 0; i < N + a - 1 && i < Nmax; i++)
+   // s - размер массива A, с учетом добавленных элементов
+   UCHAR s = N;
+   for (UCHAR i = 0; i < s - 1 && i < Nmax; i++)
    {
       if (A[i] == X)
       {
-         // d - количество дополнительных элементов при серии
-         short d = 0;
-         for (; A[i + d + 1] == X; d++);
+         UCHAR c = 1, l_in = i;
+         for ( ; A[i + c] == X; c++, l_in++);
 
-         // перемещаем "хвост" массива вправо на 1 позицию
-         // Если текущее кол-во элементов массива больше допустимого, то j = Nmax - 1 (максимальное допустимое значение), иначе j = N + a (последний элемент в массиве)
-         for (UCHAR j = N + a < Nmax - 1 ? N + a : Nmax - 1; j > i + d; j--)
-            A[j] = A[j - 1 - d];
+         for (UCHAR j = s + c - 1; l_in + c < s - 1 && j > l_in; j--) A[j] = A[j - c];
 
-         // Дублируем
-         for (; d >= 0; d--)
-         {
-            A[i + d + 1] = A[i];
-            i += 2;
-            a++;
-         }
+         for (UCHAR k = c; k > 0; k--, i+= 2) A[i + k] = X;
+
+         s = s + c > Nmax ? Nmax : s + c;
       }
    }
 
-   return N + a < Nmax ? N + a : Nmax;
+   return s;
 }
-void PrintArray(short* A, UCHAR N)
+void PrintArray(UCHAR *A, UCHAR N)
 {
    printf_s("Размер массива = %hi\nЭлементы массива: ", N);
    for (size_t i = 0; i < N; i++) printf_s("%d ", A[i]);
    printf_s("\n");
 }
-void RandArray(short* A, UCHAR N)
+void RandArray(UCHAR *A, UCHAR N)
 {
    for (size_t i = 0; i < N; i++)
    {
-      short r = rand() % Rmax + Rmin;
+      UCHAR r = rand() % Rmax + Rmin;
       if (r % 2) // если так, то генерируем серию чисел случайной длины
       {
          // генерируем число от Lmin до Lmax (длина серии)
-         for (short l = rand() % Lmax + Lmin; l > 0 && i < N; l--, i++)
+         for (short l = rand() % (Lmax + 1 - Lmin) + Lmin; l > 0 && i < N; l--, i++)
             A[i] = r;
          // уменьшаем значение счетчика, так как после генерации серии i пропускает одну позицию
          i--;
